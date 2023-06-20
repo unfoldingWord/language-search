b3d088210aedf1eea5279019348bed77a99de27d
yakob-aleksandrovich <jakobaleksandrovich@gmail.com>
2023-06-20 12:10:29 +0200

    Reworked script
    - Upgraded to use Python3
    - Proper logging
    - If loading from URL not possible, then load from local file (not TOWA)
    - Fixed several small bugs
    - Correctly sort output
    - Renamed autocomplete.py to main.py

--- main.py
+++ main.py
@@ -25,27 +25,48 @@
 #  Contributors:
 #  Patrick Altman <paltman@eldarion.com>
 #  Jesse Griffin <jesse@distantshores.org>
+#  Jaap-Jan Swijnenburg <jaap-jan.swijnenburg@unfoldingword.org>
 
 import sys
 import json
-import urllib2
 
 from datetime import timedelta
 from functools import update_wrapper
 
-from flask import Flask, request, jsonify, make_response, current_app
-
+from flask import Flask, request, make_response, current_app
+from urllib.request import urlopen
+from urllib.error import HTTPError, URLError
+import logging
+from socket import timeout
 
 app = Flask(__name__)
 app.debug = True
 
+# Init logging
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
 
+# Loading langnames.json at startup
+# Initially from URL. If URL does timeout, then load from local (which might be slightly outdated)
+# Try catch flow taken from https://stackoverflow.com/a/8763542
+url = "https://td.unfoldingword.org/exports/langnames.json"
 try:
+    source = urlopen(url, timeout=10)
+    logging.info('Loading languages from URL %s', url)
+except HTTPError as error:
+    logging.error('HTTP Error: Data not retrieved because %s\nURL: %s', error, url)
+    logging.info('Loading from local file: langnames.json')
     source = open("langnames.json")
-except IOError:
-    source = urllib2.urlopen(
-        "http://td.unfoldingword.org/exports/langnames.json"
-    )
+except URLError as error:
+    if isinstance(error.reason, timeout):
+        logging.error('Timeout Error: Data not retrieved because %s\nURL: %s', error, url)
+        logging.info('Loading from local file: langnames.json')
+        source = open("langnames.json")
+    else:
+        logging.error('URL Error: Data not retrieved because %s\nURL: %s', error, url)
+        logging.info('Loading from local file: langnames.json')
+        source = open("langnames.json")
+
 data = json.loads(source.read())
 
 
@@ -54,9 +75,9 @@ def crossdomain(origin=None, methods=None, headers=None,
                 automatic_options=True):
     if methods is not None:
         methods = ", ".join(sorted(x.upper() for x in methods))
-    if headers is not None and not isinstance(headers, basestring):
+    if headers is not None and not isinstance(headers, str):
         headers = ", ".join(x.upper() for x in headers)
-    if not isinstance(origin, basestring):
+    if not isinstance(origin, str):
         origin = ", ".join(origin)
     if isinstance(max_age, timedelta):
         max_age = max_age.total_seconds()
@@ -111,7 +132,14 @@ def autocomplete():
             for x in data
             if term in x["ln"].lower() or term in x["lr"].lower() or term in x["ang"].lower()
         ])
-    return jsonify({"results": d, "count": len(d), "term": term})
+
+    full_result = {"count": len(d), "term": term, "results": d}
+    response = app.response_class(
+        response=json.dumps(full_result),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
 
 
 if __name__ == "__main__":
@@ -119,4 +147,5 @@ if __name__ == "__main__":
         port_number = int(sys.argv[1])
     except:
         port_number = 5000
+
     app.run(host='0.0.0.0', port=port_number)
